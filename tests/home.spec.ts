db54a024bdb17826b801af483f26d0ed42309dff
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-06-06 13:57:32 -0300

    iniciando projeto markL

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,7 @@
 import {test, expect} from '@playwright/test'
 
 test('webApp deve estar online', async ({page})=> {
-    await page.goto('http://127.0.0.1:8080')
-    expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
-        
+    await page.goto('http://localhost:8080/')
+    await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
 })
 
