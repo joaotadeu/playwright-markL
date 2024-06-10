59dcf5b920894f6a894d33e797ae05c6dd144753
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-06-10 18:25:10 -0300

    Custom URLs com dotenv

--- tests/support/helpers.ts
+++ tests/support/helpers.ts
@@ -1,12 +1,14 @@
 import { expect, APIRequestContext } from "@playwright/test"
 import { tarefaModel } from '../fixtures/tarefa.model'
 
+const BASE_API = process.env.BASE_API
+
 export async function deleteTarefaByHelper(request: APIRequestContext, nomeTarefa: String) {
-    await request.delete('http://localhost:3333/helper/tasks/' + nomeTarefa)
+    await request.delete(`${BASE_API}/helper/tasks/${nomeTarefa}`)
 
 }
 
 export async function postTarefa(request: APIRequestContext, tarefa: tarefaModel) {
-    const novaTarefa = await request.post('http://localhost:3333/tasks/', { data: tarefa })
+    const novaTarefa = await request.post(`${BASE_API}/tasks/`, { data: tarefa })
     expect(novaTarefa.ok()).toBeTruthy()
 }
